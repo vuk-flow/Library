b512f31d04378819fbec336e1abf536e3e76ea00
vuk <vuk.sakic@gmail.com>
2026-01-01 15:48:01 +0100

    Initial commit. Configured TS, Next, Eslint and prettier.

---
+++ backend/eslint.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const eslintConfig = defineConfig([
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  // Override default ignores
+  globalIgnores([
+    "dist/**",
+    "node_modules/**",
+  ]),
+  {
+    rules: {
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    },
+  },
+]);
+
+export default eslintConfig;
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react"
+
+const App = () => {
+
+  return (
+    <Flex>Welcome</Flex>
+  )
+}
+
+export default App;
