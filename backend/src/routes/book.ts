6f3b8bb61b486a6e9817148f10ab316173cce316
Vuk  Šakić <129513318+vuk-flow@users.noreply.github.com>
2026-01-18 11:05:18 +0100

    Added endpoint to get book by id (#8)

--- backend/src/routes/book.ts
+++ backend/src/routes/book.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import { prisma } from '../lib/prisma.js';
+import { BookWithAllInfo } from '../models/book.js';
 
 const bookRouter = Router();
 
@@ -13,6 +14,41 @@ bookRouter.get('/', async (req: Request, res: Response) => {
     res
       .status(500)
       .json({ error: `Error occured while getting all books: ${err}` });
+  }
+});
+
+bookRouter.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const book = await prisma.book.findFirst({
+      where: {
+        id: String(id),
+      },
+      include: {
+        author: true,
+        section: true,
+        library: true,
+      },
+    });
+    if (book) {
+      const bookWithAllInfo: BookWithAllInfo = {
+        id: book.id,
+        name: book.name,
+        date_published: book.published_date
+          .toISOString()
+          .split('T')[0] as string,
+        author: book.author.name,
+        section: book.section.name,
+        library: book.library.name,
+      };
+      res.json(bookWithAllInfo).status(200);
+    } else {
+      res.json({ message: `The book with ${id} not found!` });
+    }
+  } catch (err) {
+    console.error({
+      error: `Error occured while getting a book by id: ${err}`,
+    });
   }
 });
 
